5b149e7ad18b703acd6282359d5fcf9b38e02341
ananthmuppidi <ananth.muppidi21@gmail.com>
2024-02-23 12:12:51 +0530

    more code

---
+++ Q1/reducer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""reducer.py"""
+import sys
+
+current_word = None
+current_docs = []
+
+# input comes from STDIN
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    
+    # parse the input we got from mapper.py
+    word, doc_id = line.split('\t', 1)
+    
+    # this IF-switch only works because Hadoop sorts map output
+    # by key (here: word) before it is passed to the reducer
+    if current_word == word:
+        if doc_id not in current_docs:
+            current_docs.append(doc_id)
+    else:
+        if current_word:
+            # write result to STDOUT
+            print('%s\t%s' % (current_word, ' '.join(current_docs)))
+        current_docs = [doc_id]
+        current_word = word
+
+# do not forget to output the last word if needed!
+if current_word == word:
+    print('%s\t%s' % (current_word, ' '.join(current_docs)))
+
---
+++ Q1/runnerscript.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+STREAM_JAR=$1
+LOCAL_INP=$2
+HDFS_INP=$3
+HDFS_OUT=$4
+FILES=$5
+
+# put files on hdfs
+
+hdfs dfs -rm -r ${HDFS_INP}example_input ${HDFS_OUT}example_output/
+hdfs dfs -mkdir -p ${HDFS_INP}example_input/
+hdfs dfs -put ${LOCAL_INP} ${HDFS_INP}example_input/
+
+hadoop jar $STREAM_JAR \
+-D mapred.reduce.tasks=3 \
+-input ${HDFS_INP}example_input/ \
+-output $HDFS_OUT/example_output/ \
+-mapper ${FILES}mapper.py \
+-file ${FILES}mapper.py \
+-reducer ${FILES}reducer.py \
+-file ${FILES}reducer.py
